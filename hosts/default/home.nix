395ed5ffa4fd326f25af10d2dd53638b3caada0e
wer-zen <zenwerden@gmail.com>
2025-06-29 16:53:49 +0200

    huh

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -2,7 +2,7 @@
 
 let 
   
-  packagmes = import ../../home/packages.nix { inherit pkgs; };
+  flows = import ../../home/packages.nix { inherit pkgs; };
 
 in
 {
@@ -17,13 +17,14 @@ in
 
     # Programs!
     ../../home/modules/cli/spicetify/spicetify.nix
+    ../../home/modules/desktop/quickshell/quickshell.nix
     ../../home/modules/development/git/git.nix
     ../../home/modules/development/shell/fish.nix
     # Inputs
     inputs.spicetify-nix.homeManagerModules.default
   ];
 
-  home.packages = packagmes;
+  home.packages = flows;
     
 
   services.cliphist = {
