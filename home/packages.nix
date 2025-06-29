98b721f0ec7f3ba032dacc03b9f2fff16d439754
wer-zen <zenwerden@gmail.com>
2025-06-29 08:55:47 +0200

    homepkgs

---
+++ home/packages.nix
@@ -0,0 +1,56 @@
+{ pkgs, ...}:
+
+with pkgs; 
+[
+    # >>"Hello, world!" when run.<<
+    hello
+  
+    # >>DEV<<
+    zed-editor
+    rustup
+    tmux
+    ghostty
+    fish
+    git
+    kitty
+
+    # >>MUSIC<<
+    easyeffects
+    cava
+
+    # >>NAVIGATION<<
+    fzf
+    ulauncher
+    zoxide
+    yazi
+    wofi
+    eza
+    nautilus
+
+    # >>SYSTEM UTILITIES<<
+    wget
+    mission-center
+    unzip
+    killall
+    tree
+
+    # >>IDK<<
+    vesktop
+    swaynotificationcenter
+
+    # >>WALLPAPER & CUSTOMIZATION & SCREENUTILS<<
+    hyprshot
+    grim
+    fastfetch
+    swww
+    spicetify-cli
+    slurp
+    matugen
+    gpu-screen-recorder
+
+    # >>QUICKSHILL<<
+    qt6Packages.qt5compat
+    libsForQt5.qt5.qtgraphicaleffects
+    kdePackages.qtbase
+    kdePackages.qtdeclarative
+]
