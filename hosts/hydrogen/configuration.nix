d0686e09aece01c45ddeff32f27da7c7096b78bb
wer-zen <zenwerden@gmail.com>
2025-06-30 08:54:38 +0200

    hydrogen host

---
+++ hosts/hydrogen/configuration.nix
@@ -0,0 +1,145 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
+
+  services.power-profiles-daemon.enable = false;
+  systemd.user.units.swaync.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  xdg.portal.enable = true;
+
+  environment.sessionVariables = {
+
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware.graphics.enable = true;
+
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "flow"; # Define your hostname.
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Rome";
+
+  fonts = {
+    enableDefaultPackages = true;
+    fontconfig = {
+      enable = true;
+      defaultFonts.monospace = [ "nerd-fonts.jetbrains-mono" ];
+    };
+    packages = with pkgs; [
+      fira-sans
+      fira-code
+      roboto
+      jetbrains-mono
+      noto-fonts
+      noto-fonts-emoji
+      material-symbols
+      material-icons
+    ];
+  };
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "it_IT.UTF-8";
+    LC_IDENTIFICATION = "it_IT.UTF-8";
+    LC_MEASUREMENT = "it_IT.UTF-8";
+    LC_MONETARY = "it_IT.UTF-8";
+    LC_NAME = "it_IT.UTF-8";
+    LC_NUMERIC = "it_IT.UTF-8";
+    LC_PAPER = "it_IT.UTF-8";
+    LC_TELEPHONE = "it_IT.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = false;
+  services.displayManager.ly.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.zen = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = false;
+  services.displayManager.autoLogin.user = "zen";
+  services.mpd.enable = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  services.auto-cpufreq = { enable = true; };
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    neovim
+    ly
+    home-manager
+    inputs.zen-browser.packages.x86_64-linux.default
+  ];
+
+  system.stateVersion = "25.05";
+
+}
---
+++ hosts/hydrogen/home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, inputs, ... }:
+
+let flows = import ../../home/packages.nix { inherit pkgs; };
+in {
+  home.username = "zen";
+  home.homeDirectory = "/home/zen";
+  home.stateVersion = "25.05";
+  nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    # Programs!
+    ../../home/modules/default.nix
+  ];
+  home.packages = flows;
+  services.cliphist = {
+    enable = true;
+    allowImages = true;
+  };
+  home.file = { };
+  home.sessionVariables = {
+    # arise
+  };
+
+  zen = {
+    modules = {
+      desktop.quickshell.enable = true;
+      cli.spicetify.enable = true;
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
