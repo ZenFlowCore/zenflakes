67ae15e7dd2732086430fe6f5c9e02014f4e019f
wer-zen <zenwerden@gmail.com>
2025-07-07 06:06:16 +0200

    foot

--- hosts/oxygen/home.nix
+++ hosts/oxygen/home.nix
@@ -33,6 +33,7 @@ in {
     };
     desktop.apps.obsidian.enable = true;
     terminal.tmux.enable = true;
+    terminal.foot.foot.enable = true;
     terminal.kitty.kitty.enable = true;
     terminal.oh-my-posh.oh-my-posh.enable = true;
   };
