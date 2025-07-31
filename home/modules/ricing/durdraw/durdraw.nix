75c5002caafe91df8f82adaa43df8287d3a0e9fc
wer-zen <zenwerden@gmail.com>
2025-07-31 10:36:22 +0200

    durdraw??!?!?!?!

--- home/modules/ricing/durdraw/durdraw.nix
+++ home/modules/ricing/durdraw/durdraw.nix
@@ -1,25 +1,11 @@
 { config, lib, inputs, pkgs, ... }:
 with lib;
-let
-  cfg = config.zen.modules.ricing.durdraw.durdraw;
-
-  durdraw = let
-    python = pkgs.python3;
-    pythonPackages = python.pkgs;
-    projectData = builtins.fromTOML (builtins.readFile ./pyproject.toml);
-  in pythonPackages.buildPythonApplication {
-    pname = projectData.project.name;
-    version = projectData.project.version;
-    src = ./.;
-    pyproject = true;
-    nativeBuildInputs = [ pythonPackages.setuptools pythonPackages.wheel ];
-    buildInputs = [ pkgs.neofetch pkgs.ansilove ];
-    doCheck = false;
-  };
+let cfg = config.zen.modules.ricing.durdraw.durdraw;
 in {
   options.zen.modules.ricing.durdraw.durdraw = {
     enable = mkEnableOption "Enable durdraw ASCII art editor";
   };
-
-  config = mkIf cfg.enable { home.packages = [ durdraw ]; };
+  config = mkIf cfg.enable {
+    home.packages = [ inputs.durdraw.${pkgs.system}.default ];
+  };
 }
