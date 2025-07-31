148eb361a2afa3c64d4a93febc637c1543b7e045
wer-zen <zenwerden@gmail.com>
2025-07-31 09:52:27 +0200

    durdraw??!?!?!?!

--- home/modules/ricing/durdraw/durdraw.nix
+++ home/modules/ricing/durdraw/durdraw.nix
@@ -1,14 +1,25 @@
 { config, lib, inputs, pkgs, ... }:
-
 with lib;
-let cfg = config.zen.modules.ricing.durdraw;
-in {
+let
+  cfg = config.zen.modules.ricing.durdraw.durdraw;
 
-  options.zen.modules.ricing.durdraw = { enable = mkEnableOption "heh"; };
-
-  config = mkIf cfg.enable {
-
-    home.packages = [ ./flake.nix ];
-
+  durdraw = let
+    python = pkgs.python3;
+    pythonPackages = python.pkgs;
+    projectData = builtins.fromTOML (builtins.readFile ./pyproject.toml);
+  in pythonPackages.buildPythonApplication {
+    pname = projectData.project.name;
+    version = projectData.project.version;
+    src = ./.;
+    pyproject = true;
+    nativeBuildInputs = [ pythonPackages.setuptools pythonPackages.wheel ];
+    buildInputs = [ pkgs.neofetch pkgs.ansilove ];
+    doCheck = false;
   };
+in {
+  options.zen.modules.ricing.durdraw.durdraw = {
+    enable = mkEnableOption "Enable durdraw ASCII art editor";
+  };
+
+  config = mkIf cfg.enable { home.packages = [ durdraw ]; };
 }
